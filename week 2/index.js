e8e097a176564eb6cbc02b39faf09d6de8448c62
rudiafandiw <rudiafandiw@gmail.com>
2019-10-07 21:59:40 +0700

    pertemuan week 2

---
+++ week 2/index.js
@@ -0,0 +1,36 @@
+var angka = 1;
+
+while (angka < 11) {
+  console.log('ini angka ke', angka);
+  angka++;
+}
+console.log('berhitung selesai');
+
+ // ATAU 
+
+ var angka = 1
+
+ while (angka < 10) {
+     console.log('ini angke ke', angka);
+     angka += 2;
+ }
+
+for (var angka = 1;angka < 11; angka++)
+  console.log('berhitung selesai', angka);var angka = 1;
+
+var count = 1
+
+while (count < 11){
+  console.log('Count', count);
+  count++;
+  if (count % 2)
+    console.log('even') 
+  else {console.log ('odd')}
+}
+
+var perhitungan = 1
+
+while (perhitungan < 100){
+  perhitungan ++;
+}
+console.log('hasil penjumlahan', perhitungan);
